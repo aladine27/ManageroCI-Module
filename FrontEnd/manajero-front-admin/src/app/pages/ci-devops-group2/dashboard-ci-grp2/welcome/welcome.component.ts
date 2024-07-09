08e0a8f72a4e634294a878d017cd611a75287732
sirinebenyounes <125454731+sirinebenyounes@users.noreply.github.com>
2024-07-09 22:50:20 +0100

    CORS fix

--- FrontEnd/manajero-front-admin/src/app/pages/ci-devops-group2/dashboard-ci-grp2/welcome/welcome.component.ts
+++ FrontEnd/manajero-front-admin/src/app/pages/ci-devops-group2/dashboard-ci-grp2/welcome/welcome.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Tutorial } from '../../../../models/tutorial.model';
+import { TutorialService } from '../../../../services/tutorial.service';
 
 @Component({
   selector: 'ngx-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
+  tutorials: Tutorial[] = [];
 
+  constructor(private tutorialService: TutorialService) { }
+
+  ngOnInit(): void {
+    this.tutorialService.getAll().subscribe((data: Tutorial[]) => {
+      this.tutorials = data;
+      console.log(this.tutorials);
+    });
+  }
 }
