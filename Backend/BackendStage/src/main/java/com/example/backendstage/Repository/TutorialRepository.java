e1d5a74609110ef690ff4c49319c0ee79e90a036
alaedineibrahim <alaedineibrahim@gmail.com>
2024-07-19 10:04:01 +0100

    fix

--- Backend/BackendStage/src/main/java/com/example/backendstage/Repository/TutorialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backendstage.Repository;
-
-import com.example.backendstage.Entity.Tutorial;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TutorialRepository extends MongoRepository<Tutorial, String> {
-}
