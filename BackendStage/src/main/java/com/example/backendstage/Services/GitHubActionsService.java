baa224a910db30c76d9bf03088c2da310310cccb
sirinebenyounes <125454731+sirinebenyounes@users.noreply.github.com>
2024-08-01 12:12:47 +0100

    update service

--- BackendStage/src/main/java/com/example/backendstage/Services/GitHubActionsService.java
+++ BackendStage/src/main/java/com/example/backendstage/Services/GitHubActionsService.java
@@ -31,7 +31,10 @@ public class GitHubActionsService {
             headers.set("Authorization", "token " + token);
             headers.set("Accept", "application/vnd.github.v3+json");
 
-            String body = "{\"ref\": \"main\"}"; // Assurez-vous que 'main' est la branche correcte
+            String body = "{\"ref\": \"main\"}";
+
+            System.out.println("Triggering build with URL: " + workflowUrl);
+            System.out.println("Headers: " + headers.toString());
 
             HttpEntity<String> entity = new HttpEntity<>(body, headers);
             return restTemplate.exchange(workflowUrl, HttpMethod.POST, entity, String.class);
